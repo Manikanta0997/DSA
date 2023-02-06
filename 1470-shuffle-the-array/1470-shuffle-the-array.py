657ad9da7b551763ed943ec33b71e2bdc75fffa2
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-02-06 18:58:33 +0530

    Time: 46 ms (51.36%), Space: 13.6 MB (46.03%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        li =[]
-        for i in range(len(nums)/2):
+        li = []
+        for i in range(n):
             li.append(nums[i])
-            li.append(nums[len(nums)/2 + i])
+            li.append(nums[n+i])
         return li
         
