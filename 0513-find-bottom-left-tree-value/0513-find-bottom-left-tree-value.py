f1f269b4193fc9c73a3124a98b6fd832585c156e
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-05-04 13:00:23 +0530

    Time: 35 ms (46.43%), Space: 17.9 MB (67.86%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class queue:
+    def __init__(self):
+        self.li = []
+        self.top = 0
+        self.rear = 0
+        self.res = []
+    def enqueue(self, root):
+        self.li.append(root)
+        self.top = self.top + 1
+    def enqueue1(self, root):
+        self.li.append([root])
+        self.top = self.top + 1
+    def dequeue(self):
+        while(self.top > self.rear):
+            z = self.li[self.rear]
+            r = []
+            a = []
+            for d in range(len(z)):
+                i = z[d]
+                r.append(i.val)
+                if(i.left != None):
+                    a.append(i.left)
+                if(i.right != None):
+                    a.append(i.right)
+            self.res.append(r)
+            self.rear = self.rear + 1
+            if(a != []):
+                self.enqueue(a)
+        return self.res
+class Solution(object):
+    def findBottomLeftValue(self, root):
+        if(root != None):
+            x = queue()
+            x.enqueue1(root)
+            y = x.dequeue()
+            return y[len(y)-1][0]
+        
+        
