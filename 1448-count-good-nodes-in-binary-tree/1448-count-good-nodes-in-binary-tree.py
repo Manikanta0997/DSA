5ddc4086469a5f6e7dca9ebfd5a620ce4b69d217
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-08-19 13:00:08 +0530

    Time: 203 ms (76.94%), Space: 49.1 MB (57.61%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,20 +6,16 @@
 #         self.right = right
 class Solution(object):
     def goodNodes(self, root):
-        def countgood(root, maxval):
+        def recu(root, maxval):
             if root == None:
                 return 0
             if root.val >= maxval:
-                res =  1
+                res = 1
             else:
-                res =  0
-            maxval = max(root.val, maxval)
-            res += countgood(root.left, maxval)
-            res += countgood(root.right, maxval)
+                res = 0
+            maxval = max(maxval, root.val)
+            res += recu(root.left, maxval)
+            res += recu(root.right, maxval)
             return res
-        return countgood(root, root.val)
-        
-            
-            
-            
+        return recu(root, root.val)
         
