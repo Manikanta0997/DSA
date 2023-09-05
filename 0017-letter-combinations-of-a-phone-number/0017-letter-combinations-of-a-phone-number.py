2adf0a64e8004e63b5ac88689324c84107eb35ec
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-09-05 17:53:48 +0530

    Time: 49 ms (9.46%), Space: 16.5 MB (14.01%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,29 +1,33 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        li = [[],[],['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'], ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']]
-        res = []
-        li1 = []
-        if(len(digits) != 0):
-            for i in range(len(digits)):
-                x = int(digits[i:i+1])
-                li1.append(li[x])
-            li2 = []
-            li2.append(li1[0])
-            for i in range(len(li1)-1):
-                y = []
-                for j in range(len(li2[i])):
-                    for k in range(len(li1[i+1])):
-                        x = li2[i][j] + li1[i+1][k]
-                        y.append(x)
-                li2.append(y)
-            if(len(digits)> 1):
-                return y
-            else:
-                return li1[0]
-            
-            
-            
-                
-            
-            
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) > 0:
+            res = []
+            two = 'abc'
+            three = 'def'
+            four = 'ghi'
+            five = 'jkl'
+            six = 'mno'
+            seven = 'pqrs'
+            eight = 'tuv'
+            nine = 'wxyz'
+            li = [two, three, four, five, six, seven, eight, nine]
+            li_index = ['2', '3', '4', '5', '6', '7', '8', '9']
+            x = digits[:1]
+            for i in range(len(li[li_index.index(x)])):
+                res.append(li[li_index.index(x)][i:i+1])
+
+            for i in range(len(digits)-1):
+                k = i + 1
+                li1 = []
+                x = digits[k:k+1]
+                for l in range(len(li[li_index.index(x)])):
+                    li1.append(li[li_index.index(x)][l:l+1])
+                temp = []
+                for z in range(len(res)):
+                    for g in range(len(li1)):
+                        temp.append(res[z] + li1[g])
+                res = temp
+            return res
+
+        
         
