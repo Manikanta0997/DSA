e34d493dc1954101c04b42a224cb62ce867567ef
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-09-12 22:33:37 +0530

    Time: 504 ms (14.68%), Space: 17.9 MB (35.00%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -1,13 +1,18 @@
-class Solution(object):
-    def removeStars(self, s):
-        li = []
+class Solution:
+    def removeStars(self, s: str) -> str:
+        res = ""
+        li =[]
+        top = 0
         for i in range(len(s)):
-            li.append(s[i:i+1])
-        x = ''
-        for i in range(len(li)):
-            if(li[i] != '*'):
-                x = x + li[i]
-            if(li[i] == '*'):
-                x = x[0:len(x)-1]
-        return x
-        
+            if s[i:i+1] != '*':
+                if top < len(li):
+                    li[top] = s[i:i+1]
+                else:
+                    li.append(s[i:i+1])
+                top += 1
+            else:
+                top -= 1
+        for i in range(top):
+            res += li[i]
+        return res
+                    
