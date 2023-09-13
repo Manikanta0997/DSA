2b29b025921cd66d2612eacb9704f00c0ce53c60
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-09-13 17:17:58 +0530

    Time: 68 ms (51.17%), Space: 16.3 MB (62.49%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,50 +1,27 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        str1 = ""
-        ptr = l1
-        ret = l1
-        str2 = ""
-        while(l1.next != None):
-            str1 = str1 + str(l1.val)
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        str1 = ''
+        str2 = ''
+        while l1:
+            str1 = str(l1.val) + str1 
             l1 = l1.next
-        str1 = str1 + str(l1.val)
-        while(l2.next != None):
-            str2 = str2 + str(l2.val)
+        while l2:
+            str2 = str(l2.val) + str2
             l2 = l2.next
-        str2 = str2 + str(l2.val)
-        str1 = str1[::-1]
-        str2 = str2[::-1]
-        add = str(int(str1) + int(str2))
-        add = int(add)
-        li = []
-        while(add > 0):
-            li.append(add%10)
-            add = add / 10
-        print(li)
-        print(ptr.val)
-        count = 0
-        for i in range(len(li)):
-            if(ptr.next == None):
-                if(count == 0):
-                    ptr.val = li[i]
-                    count = count + 1
-                else:
-                    newnode = ListNode(li[i])
-                    ptr.next = newnode
-                    ptr = newnode
-            else:
-                ptr.val = li[i]
-                ptr = ptr.next
-        return ret
-        
-                
+        res = str(int(str1) + int(str2))[::-1]
+        head = ListNode()
+        x = head
+        for i in range(len(res)):
+            head.val = int(res[i:i+1])
+            if i != len(res) - 1: 
+                head.next = ListNode()
+                head = head.next
+        return x
+            
             
         
-            
-            
-            
