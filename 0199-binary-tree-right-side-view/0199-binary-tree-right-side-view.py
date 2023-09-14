c20886006b1c4980cf83681a483c6c5eeeebfe48
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-09-14 16:17:49 +0530

    Time: 43 ms (46.43%), Space: 16.2 MB (73.77%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -1,48 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = collections.deque()
+        if root:
+            q.append(root)
 
-class queue:
-    def __init__(self):
-        self.li = []
-        self.top = 0
-        self.rear = 0
-        self.res = []
-    def enqueue(self, root):
-        self.li.append(root)
-        self.top = self.top + 1
-    def enqueue1(self, root):
-        self.li.append([root])
-        self.top = self.top + 1
-    def dequeue(self):
-        while(self.top > self.rear):
-            z = self.li[self.rear]
-            r = []
-            a = []
-            for d in range(len(z)):
-                i = z[d]
-                r.append(i.val)
-                if(i.left != None):
-                    a.append(i.left)
-                if(i.right != None):
-                    a.append(i.right)
-            self.res.append(r)
-            self.rear = self.rear + 1
-            if(a != []):
-                self.enqueue(a)
-        return self.res
-                
-class Solution(object):
-    def rightSideView(self, root):
-        if(root != None):
-            x = queue()
-            x.enqueue1(root)
-            y = x.dequeue()
-            res = []
-            for i in range(len(y)):
-                res.append(y[i][len(y[i])-1])
-            return res
-        
+        while q:
+            val = []
+
+            for i in range(len(q)):
+                node = q.popleft()
+                val.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(val)
+        for i in range(len(res)):
+            res[i] = res[i][len(res[i])-1]
+        return res
+                    
